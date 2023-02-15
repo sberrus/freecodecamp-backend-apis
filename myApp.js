64c0977a7e6abc4a59eda271a7cab1c2f2a5fb63
samdev <samuelberrus@gmail.com>
2023-02-15 14:01:43 +0100

    serve public folder

--- myApp.js
+++ myApp.js
@@ -1,8 +1,6 @@
 let express = require("express");
 let app = express();
 
-app.get("/", function (req, res) {
-	res.sendFile(__dirname+ "/views/index.html");
-});
+app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
