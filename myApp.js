119cf1639a63b37293b89e6703a0908b9044a1e9
samdev <samuelberrus@gmail.com>
2023-02-15 15:10:49 +0100

    Add time to response appending the time to req.time property

--- myApp.js
+++ myApp.js
@@ -27,4 +27,15 @@ app.get("/json", function (req, res) {
 		res.json({ message: "Hello json" });
 	}
 });
+
+app.get(
+	"/now",
+	(req, res, next) => {
+		req.time = new Date().toString();
+		next();
+	},
+	(req, res) => {
+		res.json({ time: req.time });
+	}
+);
 module.exports = app;
