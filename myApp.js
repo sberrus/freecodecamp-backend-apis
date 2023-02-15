8eaa091eaf829f2dae9ea3a4020f032344dea4f6
samdev <samuelberrus@gmail.com>
2023-02-15 14:24:01 +0100

    Add logic with env variables

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let app = express();
+require("dotenv").config();
 
 app.use("/public", express.static(__dirname + "/public"));
 
@@ -8,7 +9,11 @@ app.get("/", function (req, res) {
 });
 
 app.get("/json", function (req, res) {
-	res.json({ message: "Hello json" });
+	if (process.env.MESSAGE_STYLE === "uppercase") {
+		res.json({ message: "Hello json".toUpperCase() });
+	} else {
+		res.json({ message: "Hello json without uppercase" });
+	}
 });
 
 module.exports = app;
