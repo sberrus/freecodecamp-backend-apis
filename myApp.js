482cd667bab5dbced798d855646b1f2e3e1917c6
samdev <samuelberrus@gmail.com>
2023-02-15 13:57:26 +0100

    serve index.html file

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require("express");
 let app = express();
 
 app.get("/", function (req, res) {
-	res.send("Hello Express");
+	res.sendFile(__dirname+ "/views/index.html");
 });
 
 module.exports = app;
