1d393d74900a26f77fbd1fb8315c63d85d1dc9ce
samdev <samuelberrus@gmail.com>
2023-02-15 14:17:20 +0100

    Create a get route for /json and response

--- myApp.js
+++ myApp.js
@@ -1,10 +1,14 @@
 let express = require("express");
 let app = express();
 
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", function (req, res) {
 	res.sendFile(__dirname + "/views/index.html");
 });
 
-app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", function (req, res) {
+	res.json({ message: "Hello json" });
+});
 
 module.exports = app;
