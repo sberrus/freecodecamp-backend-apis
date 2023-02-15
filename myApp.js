f11810a8152d4ba1d0d39823214342c05453c3fa
samdev <samuelberrus@gmail.com>
2023-02-15 15:04:51 +0100

    Add logger to express

--- myApp.js
+++ myApp.js
@@ -3,9 +3,20 @@ require("dotenv").config();
 let app = express();
 require("dotenv").config();
 
+app.use((req, res, next) => {
+	const path = req.path;
+	const method = req.method;
+	const ip = req.ip;
+
+	console.log(`${method} ${path} - ${ip}`);
+
+	next();
+});
+
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
+	console.log("hola");
 	res.sendFile(__dirname + "/views/index.html");
 });
 
@@ -13,8 +24,7 @@ app.get("/json", function (req, res) {
 	if (process.env.MESSAGE_STYLE === "uppercase") {
 		res.json({ message: "Hello json".toUpperCase() });
 	} else {
-		res.json({ message: "Hello json without uppercase" });
+		res.json({ message: "Hello json" });
 	}
 });
-
 module.exports = app;
